797da91d772fb1c031a20afc8807cda5323b5298
emiradu98 <emiradu98@icloud.com>
2020-02-08 19:01:56 +0200

    working on fe

---
+++ frontEnd/src/components/molecules/FormInput/FormSelect.js
@@ -0,0 +1,32 @@
+import './FormInput.scss';
+
+export class FormInput {
+    constructor({name, placeholder, id, labelName, onChange, type, required}) {
+        this.label = document.createElement('label');
+        this.label.htmlFor = id;
+        this.label.innerText = labelName;
+
+        this.input = document.createElement('input');
+        this.input.name = name;
+        this.input.type = type;
+        this.input.id = id;
+        this.input.placeholder = placeholder;
+
+        this.input.required = required;
+
+        this.div = document.createElement('div');
+        this.div.appendChild(this.label);
+        this.div.appendChild(this.input);
+        this.div.className = 'formInput';
+
+        return this;
+    }
+
+    innerHTML() {
+        return this.div;
+    }
+
+    getValue() {
+        return this.input.value;
+    }
+};
